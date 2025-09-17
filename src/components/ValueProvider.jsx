01f4336ad9e7131794c5def4b48df11c74286d56
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-09-17 06:01:38 +0530

    __

--- src/components/ValueProvider.jsx
+++ src/components/ValueProvider.jsx
@@ -58,7 +58,7 @@ export default function ValueProvider({ children }) {
         if (conFirm) {
             setloading(true)
             setshowModal(false)
-            axios.post(url + "logout", {}, { withCredentials: true }).then((data) => {
+            axios.post("/api/logout", {}, { withCredentials: true }).then((data) => {
                 setloading(false);
                 localStorage.clear();
                 setuser(null);
