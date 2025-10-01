71f1f5add354c80250aae38fbf3ad51e05830e64
SatyShrey <120662129+SatyShrey@users.noreply.github.com>
2025-10-01 15:15:37 +0530

    __

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { BiSolidSend } from "react-icons/bi";
 import { useValues } from "./ValueProvider";
 import axios from "axios";
@@ -8,7 +8,7 @@ export default function InputBox() {
     const [prompt, setprompt] = useState();
     const { setloading, setcurrentChat, setuser } = useValues();
 
-    function autoLogout(){
+    function autoLogout() {
         localStorage.clear();
         setuser(null);
         toast.error("Login expired")
@@ -16,22 +16,27 @@ export default function InputBox() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(!prompt){return }
+        if (!prompt) { return }
         setloading(true);
         axios.post("/api/gemini", { prompt }, { withCredentials: true }).then(data => {
-             setloading(false);
-             if(data.data==="Server error"){return toast.error(data.data)}
-             if(data.data==="Login expired"){return autoLogout()}
-             setcurrentChat({ user: prompt, bot: data.data, id: Date.now() });
-             e.target.reset();setprompt('')
-        }).catch(err => {toast.error(err.message);setloading(false)})
+            setloading(false);
+            if (data.data === "Server error") { return toast.error(data.data) }
+            if (data.data === "Login expired") { return autoLogout() }
+            setcurrentChat({ user: prompt, bot: data.data, id: Date.now() });
+            e.target.reset(); setprompt('')
+        }).catch(err => { toast.error(err.message); setloading(false) })
     }
+
+    useEffect(() => {
+        const input = document.getElementById('myInput');
+        input.focus({ preventScroll: true });
+    }, [])
 
     return (
         <div className="p-1 shadow-[0_0_2px] bg-base-200">
             <form onSubmit={handleSubmit}
                 className='flex w-full items-center p-2 rounded overflow-hidden shadow-[0_0_2px] bg-base-100'>
-                <textarea className='h-full resize-none w-full border-none outline-none'
+                <textarea id="myInput" className='h-full resize-none w-full border-none outline-none'
                     onChange={(e) => setprompt(e.target.value)} placeholder="Ask anything..."></textarea>
                 {prompt && <button className="cursor-pointer hover:scale-110" type="submit"><BiSolidSend size={30} /></button>}
             </form>
